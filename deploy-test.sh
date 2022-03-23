ff716fb75861e2d18000fe344be5bfe45b84a87e
Max Hegler <mhegler@miva.com>
2022-03-23 11:00:07 -0700

    updates

--- deploy-test.sh
+++ deploy-test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 source .env
+
 sh build.sh
-twine upload -u $TWINE_USERNAME -p $TWINE_PASSWORD -r testpypi dist/*
+pipenv run twine upload -u $TWINE_USERNAME -p $TWINE_PASSWORD -r testpypi dist/*
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 source .env
+
 sh build.sh
-twine upload -u $TWINE_USERNAME -p $TWINE_PASSWORD dist/*
+pipenv run twine upload -u $TWINE_USERNAME -p $TWINE_PASSWORD dist/*
